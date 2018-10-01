0252ba48eb6a5ea065684b8aa2578b402c59ec75
Anthony Makepeace <makepeaceanthony@gmail.com>
2018-10-01 21:30:35 +0100

    Tidy up with bug tweaks and compartmentalisation

--- src/ConsonantAddition.java
+++ src/ConsonantAddition.java
@@ -4,7 +4,7 @@ public class ConsonantAddition {
 
     public static void main(String[] args) {
 
-
+        // Argument check
         if (args.length != 2) {
             System.out.println("Expected 2 command line arguments, but got " + args.length + ".");
             System.out.println("Please provide the path to the dictionary file as the first argument and a sentence "
@@ -12,20 +12,17 @@ public class ConsonantAddition {
             System.exit(0);
         }
 
+        // Creation of dictionary Arraylist from source file
         ArrayList<String> lines = FileUtil.readLines(args[0]);
+
+        //Make the whole of the dictionary lowercase so that words can be tested lowercase
+        ArrayList<String> dictionary = parseDictionary(lines);
 
         // If lines is empty, then display "Invalid dictionary, aborting." and exit!
         if (lines.size() == 0) {
             System.out.println("Invalid dictionary, aborting.");
             System.exit(0);
         }
-
-        //Make the whole of the dictionary lowercase so that only lowercase words need to be tested
-        for (int j = 0; j < lines.size(); j++) {
-            String oldItem = lines.get(j);
-            lines.set(j, oldItem.toLowerCase());
-        }
-
 
         // Take word or phrase and save it to "input"
         String input = args[1];
@@ -37,59 +34,76 @@ public class ConsonantAddition {
 
         for(int i=0; i<input.length()+1; i++) {
 
-            // Add the input into a string builder
-            StringBuilder sb = new StringBuilder(input);
-
-            // Add a letter into each
+            // Iterate through consonants to add into each index
             for(String letter : consonants ) {
+
+                // Insert the consonant into the phrase
+                StringBuilder sb = new StringBuilder(input);
                 sb.insert(i, letter);
                 String newInput = sb.toString();
-
-                System.out.println("DEBUG: " + newInput);
 
                 //Split string into words
                 String[] words = newInput.split(" ");
 
-                //Test if the words are contained in the dictionary
-                int counter = 0;
-                for (int x = 0; x < words.length; x++) { // Look at each word and check if its in the dictionary
-
-                    words[x] = words[x].replaceAll("[^\\w]", ""); // remove punctuation
-
-                    ////System.out.println("DEBUG: Searched word = "+ words[x]);
-                    if (lines.contains(words[x].toLowerCase())) {
-
-                        ////System.out.println("DEBUG: Found word location = "+ lines.indexOf(words[x]));
-                        counter++; // if the word is in the dictionary add one to the counter
-                    }
-
-
-                    /*
-                    else {
-                        ////System.out.println("DEBUG: Word not found!");
-                    }
-                    */
-                }
-
                 // If all the words were validated
-                ////System.out.println("DEBUG: Counter = " + counter);
-                ////System.out.println("DEBUG: Length = "+words.length);
-                if (counter == words.length) {
-                    System.out.println(newInput);
+                if (areWordsInDic(words, dictionary)) {
+                    System.out.println(newInput); // Print out the line
                     numOfAlternativesFound++; // Add one to the counter
                 }
-
-
-                //if so then print that phrase and increment the counter
-
-                // reset the phrase
-                sb.deleteCharAt(i);
             }
-
-
-
         }
 
+        //If no alternatives found print phrase, else tell how many have been found
+        if (numOfAlternativesFound == 0) {
+            System.out.println("Could not find any alternatives.");
+        } else {
+            System.out.println("Found " + numOfAlternativesFound + " alternatives.");
+        }
+    }
 
+    /**
+     * Takes the dictionary array lines and makes it all lowercase.
+     * @param lines Arraylist containing words of a dictionary
+     * @return Returns the dictionary with all words lowercase
+     */
+    public static ArrayList<String> parseDictionary(ArrayList<String> lines) {
+
+        for (int j = 0; j < lines.size(); j++) {
+            String oldItem = lines.get(j);
+            lines.set(j, oldItem.toLowerCase());
+        }
+
+        return lines;
+    }
+
+    /**
+     * Given a list of words to test against a dictionary, the method splits the words, removes irrelevant punctuation
+     * and tests whether the word exists within the dictionary.
+     * @param words String array of words to check against the dictionary
+     * @param dictionary Arraylist of strings forming a dictionary to test against
+     * @return Returns True if ALL of the words are found in the dictionary, otherwise returns false.
+     */
+    public static boolean areWordsInDic(String[] words, ArrayList<String> dictionary) {
+        // 'word is in the dictionary' counter
+        int counter = 0;
+
+        // Iterate through words in the array
+        for (int x = 0; x < words.length; x++) {
+
+            //Get the last character of word
+            char lastCharacter = words[x].charAt(words[x].length() - 1);
+
+            //Check if the last character is a '.' ',' '/' '?' '!', if so remove punctuation
+            if (".,/?!".indexOf(lastCharacter) != -1) {
+                words[x] = words[x].replaceAll("[^\\w]", ""); // remove punctuation
+            }
+
+            if (dictionary.contains(words[x].toLowerCase())) {
+                counter++; // if the word is in the dictionary add one to the counter
+            }
+        }
+
+        // If all the words are in the dictionary return true, else false
+        return counter == words.length;
     }
 }
